cd2a4245a375a929dde85fa37470d402a2bf8804
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-03-16 10:01:39 +0545

    type conversion

--- day1.py
+++ day1.py
@@ -1,22 +1,35 @@
-print("Hello world")
+# print("Hello world")
 
-#orint number
-print(24)
+# #orint number
+# print(24)
 
-'''This is multiline comment'''
-print("Multi Line comment")
+# '''This is multiline comment'''
+# print("Multi Line comment")
 
-num1=10
-num2=2
+# num1=10
+# num2=2
 
-print("Sum : ",num1+num2)
-# print("Product",product)
+# print("Sum : ",num1+num2)
+# # print("Product",product)
 
-name = "Nirdesh"
+# name = "Nirdesh"
 
-print("My name is ",name)
-name = "Ram"
-print("My name is ",name)
+# print("My name is ",name)
+# name = "Ram"
+# print("My name is ",name)
 
-a,b,c=10,12,"Hello"
+# a,b,c=10,12,"Hello"
+# print(c)
+
+a=5
+b=2.3
+
+c=a+b
 print(c)
+print(type(c))
+
+num_string = "12"
+print("Before conversion",type(num_string))
+
+num_string =int(num_string)
+print("After Conversion",type(num_string))
