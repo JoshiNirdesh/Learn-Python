17ae1bf10bf56d44fd8ed3ce07f55a393f5952b5
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-03-16 09:51:33 +0545

    basic variables

---
+++ day1.py
@@ -0,0 +1,19 @@
+print("Hello world")
+
+#orint number
+print(24)
+
+'''This is multiline comment'''
+print("Multi Line comment")
+
+num1=10
+num2=2
+
+print("Sum : ",num1+num2)
+# print("Product",product)
+
+name = "Nirdesh"
+
+print("My name is ",name)
+name = "Ram"
+print("My name is ",name)
