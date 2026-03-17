65ab6377d46d9eb85e8f1ebafe5e0e671a935831
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-03-17 10:13:00 +0545

    input and output

---
+++ excercise/operators.py
@@ -0,0 +1,10 @@
+# age = int(input("Enter a age : "))
+
+# if age>18:
+#     print("You can vote ")
+# else:
+#     print("You cannot vote")
+
+sentence = "I am a data engineer"
+
+print("data" in sentence)
