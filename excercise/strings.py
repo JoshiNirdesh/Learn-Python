ab118cf49a00ddd34aaa8084ade27c15787a8351
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-03-17 10:29:12 +0545

    strings

---
+++ excercise/strings.py
@@ -0,0 +1,10 @@
+# text = "banana"
+# print(text.count("a"))
+# print(text[::-1])
+# text = 'data engineering'
+# print(text.upper())
+# print(text.replace(" ","_"))
+
+email = "abc@gmail.com"
+domain = email.split("@")[1]
+print(domain)
