54a6415f49424cb90e5f205ed6f8684e94fd7edf
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-03-17 10:36:25 +0545

    list

---
+++ excercise/list.py
@@ -0,0 +1,13 @@
+import math
+my_list = [1,2,3,4,5,6,3]
+# print(list[0])
+# print(list[-1])
+# print(math.floor(len(list)/2))
+# list.append(10)
+# list.remove(3)
+# print(max(list))
+# print(min(list))
+# print(sum(list))
+
+lis=list(set(my_list))
+print(lis)
