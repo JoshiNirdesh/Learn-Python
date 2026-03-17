db91432f05d6b7b2cdddf9a19ed5afed3f9f27d6
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-03-17 10:10:33 +0545

    input and output

---
+++ excercise/inputandoutput.py
@@ -0,0 +1,4 @@
+total_marks = int(input("Enter a total marks: "))
+obtained_marks = int(input("Enter a obtained marks : "))
+percentage = (obtained_marks/total_marks)*100
+print("Percentage : ", percentage)
