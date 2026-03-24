3258cf5ef3c82deddb065dbdee2f911babc039d7
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-03-24 11:19:35 +0545

    postgresql connection

--- project/real-time-data-pipeline/main.py
+++ project/real-time-data-pipeline/main.py
@@ -1,5 +1,7 @@
 import requests
 import pandas as pd
+from sqlalchemy import create_engine
+
 url = "https://newsdata.io/api/1/latest?apikey=pub_4ccfa82a7fa94e56b7a99d394c12bf76"
 data  = requests.get(url).json()
 
@@ -7,4 +9,20 @@ articles = data['results']
 
 df = pd.DataFrame(articles)
 df = df[['article_id','title','pubDate','source_id','country']]
-print(df.dtypes)
+
+df['pubDate']=pd.to_datetime(df['pubDate'], errors='coerce')
+
+df['title_length']=df['title'].str.len()
+df = df.dropna()
+df=df.drop_duplicates(subset=['article_id'])
+# print(df)
+
+try:
+    engine = create_engine("postgresql+psycopg2://nirdeshzoc@localhost:5432/news")
+
+    df.to_sql("rawdata",engine,if_exists='replace',index=False)
+
+    print("Data Loaded Successfully")
+except Exception as e:
+    print("Error",e)
+
